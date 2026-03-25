c4bdd7308169f79806e5ecb38889d0ed831aa544
ishikamokati30 <mokatiis@rknec.edu>
2026-03-25 23:35:32 +0530

    Stored embeddings in MongoDB

--- backend/src/services/researchService.js
+++ backend/src/services/researchService.js
@@ -1,11 +1,22 @@
+const axios = require("axios")
 const Query = require("../models/Query")
 
 exports.processQuery = async (query) => {
 
-    const savedQuery = await Query.create({ query })
+    const response = await axios.post(
+        "http://127.0.0.1:8000/embed",
+        { text: query }
+    )
+
+    const embedding = response.data.embedding
+
+    const savedQuery = await Query.create({
+        query,
+        embedding
+    })
 
     return {
-        query: savedQuery.query,
+        query,
         processed: true,
         id: savedQuery._id
     }
